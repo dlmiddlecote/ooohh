2a6f2e317a0f50bd5fa10a4f67050c3caa78d8f1
Daniel Middlecote <daniel@ometria.com>
2020-07-08 21:33:50 +0000

    More tests

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -99,7 +99,7 @@ func (b *boardDialInfo) Validate() bool {
 	}
 
 	if strings.TrimSpace(b.BoardToken) == "" {
-		b.Errors["BoardToken"] = "Please enter a board token."
+		b.Errors["BoardToken"] = "Please enter the board's token."
 	}
 
 	return len(b.Errors) == 0
@@ -109,39 +109,45 @@ func (u *UI) GetBoard() http.Handler {
 	f, err := pkger.Open("/frontend/templates/board.html")
 	tmpl := template.Must(parseFile(f, err))
 
+	f, err = pkger.Open("/frontend/templates/error.html")
+	errTmpl := template.Must(parseFile(f, err))
+
+	type response struct {
+		Board         ooohh.Board
+		BoardDialInfo *boardDialInfo
+	}
+
+	type errResp struct {
+		Msg string
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := ooohh.BoardID(api.URLParam(r, "id"))
 
-		if r.Method != "POST" {
-			board, err := u.s.GetBoard(r.Context(), id)
-			if err != nil {
-				api.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-				return
-			}
-
-			tmpl.Execute(w, *board) //nolint:errcheck
-			return
-		}
-
+		// Retrieve the board.
 		board, err := u.s.GetBoard(r.Context(), id)
 		if err != nil {
-			api.Redirect(w, r, fmt.Sprintf("/boards/%s", id), http.StatusTemporaryRedirect)
+			msg := "Error retrieving board, please try again."
+			if errors.Is(err, ooohh.ErrBoardNotFound) {
+				msg = "Oops, the board wasn't found."
+			}
+			errTmpl.Execute(w, errResp{Msg: msg}) //nolint:errcheck
 			return
 		}
 
-		err = r.ParseForm()
-		if err != nil {
-			api.Redirect(w, r, fmt.Sprintf("/boards/%s", id), http.StatusTemporaryRedirect)
+		if r.Method == "GET" {
+			// Display the board.
+			tmpl.Execute(w, response{*board, nil}) //nolint:errcheck
 			return
 		}
 
 		body := boardDialInfo{
-			DialID:     r.FormValue("dialID"),
-			BoardToken: r.FormValue("token"),
+			DialID:     r.PostFormValue("dialID"),
+			BoardToken: r.PostFormValue("token"),
 		}
 
-		if body.DialID == "" || body.BoardToken == "" {
-			api.Redirect(w, r, fmt.Sprintf("/boards/%s", id), http.StatusTemporaryRedirect)
+		if !body.Validate() {
+			tmpl.Execute(w, response{*board, &body}) //nolint:errcheck
 			return
 		}
 
@@ -152,15 +158,22 @@ func (u *UI) GetBoard() http.Handler {
 
 		dials[len(board.Dials)] = ooohh.DialID(body.DialID)
 
-		_ = u.s.SetBoard(r.Context(), id, body.BoardToken, dials)
-
-		board, err = u.s.GetBoard(r.Context(), id)
+		err = u.s.SetBoard(r.Context(), id, body.BoardToken, dials)
 		if err != nil {
-			api.Redirect(w, r, fmt.Sprintf("/boards/%s", id), http.StatusTemporaryRedirect)
+			// add a dummy error to the body to return.
+			body.Errors["SetBoard"] = "Error adding dial, please try again."
+
+			tmpl.Execute(w, response{*board, &body}) //nolint:errcheck
 			return
 		}
 
-		tmpl.Execute(w, *board) //nolint:errcheck
+		board, err = u.s.GetBoard(r.Context(), id)
+		if err != nil {
+			errTmpl.Execute(w, errResp{Msg: "Error retrieving board, please try again."}) //nolint:errcheck
+			return
+		}
+
+		tmpl.Execute(w, response{*board, nil}) //nolint:errcheck
 
 	})
 }
--- pkg/ui/ui_test.go
+++ pkg/ui/ui_test.go
@@ -40,7 +40,7 @@ func TestIndexContainsLinkToCreateBoard(t *testing.T) {
 	s := &mock.Service{}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	// Create a new request.
 	r, err := http.NewRequest("GET", "/", nil)
@@ -80,7 +80,7 @@ func TestNewBoardContainsForm(t *testing.T) {
 	s := &mock.Service{}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	// Create a new request.
 	r, err := http.NewRequest("GET", "/new", nil)
@@ -145,7 +145,7 @@ func TestCreatingBoardOK(t *testing.T) {
 	}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	// Create a new request.
 	formData := url.Values{
@@ -180,7 +180,7 @@ func TestCreatingBoardValidation(t *testing.T) {
 	s := &mock.Service{}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	for _, tt := range []struct {
 		msg         string
@@ -255,7 +255,7 @@ func TestCreatingBoardServiceError(t *testing.T) {
 	}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	// Create a new request.
 	formData := url.Values{
@@ -321,7 +321,7 @@ func TestGetBoardContainsBoardInformation(t *testing.T) {
 	}
 
 	// Create the ui struct.
-	ui := &UI{s}
+	ui := NewUI(s)
 
 	// Create a new request.
 	r, err := newRequest("GET", "/boards/:id", nil, httprouter.Params{{Key: "id", Value: "board-id"}})
@@ -345,5 +345,436 @@ func TestGetBoardContainsBoardInformation(t *testing.T) {
 		is.True(strings.Contains(body, dial.Name))                       // dial name is in response.
 		is.True(strings.Contains(body, fmt.Sprintf("%.1f", dial.Value))) // dial value is in response.
 	}
+}
 
+func TestGetBoardContainsLinksForms(t *testing.T) {
+
+	is := is.New(t)
+
+	now := time.Now().Truncate(time.Second)
+
+	// Board that will be returned by service.
+	board := ooohh.Board{
+		ID:    ooohh.BoardID("board-id"),
+		Name:  "Testing Board",
+		Token: "token",
+		Dials: []ooohh.Dial{
+			{
+				ID:        ooohh.DialID("dial-1"),
+				Token:     "token1",
+				Name:      "Dial 1",
+				Value:     10.0,
+				UpdatedAt: now,
+			},
+			{
+				ID:        ooohh.DialID("dial-2"),
+				Token:     "token2",
+				Name:      "Dial 2",
+				Value:     66.6,
+				UpdatedAt: now,
+			},
+		},
+		UpdatedAt: now,
+	}
+
+	// Create a mock service.
+	s := &mock.Service{
+		GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+			return &board, nil
+		},
+	}
+
+	// Create the ui struct.
+	ui := NewUI(s)
+
+	// Create a new request.
+	r, err := newRequest("GET", "/boards/:id", nil, httprouter.Params{{Key: "id", Value: "board-id"}})
+	is.NoErr(err)
+
+	// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+	rr := httptest.NewRecorder()
+
+	// Invoke the get board handler.
+	ui.GetBoard().ServeHTTP(rr, r)
+
+	// Check the response status code is correct.
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Parse HTML.
+	doc, err := goquery.NewDocumentFromReader(rr.Body)
+	is.NoErr(err)
+
+	// Check the new board link is within the html.
+	found := false
+	doc.Find("a[href]").Each(func(index int, item *goquery.Selection) {
+		href, _ := item.Attr("href")
+		if href == "/new" {
+			found = true
+		}
+	})
+
+	is.True(found) // link to new board has been found.
+
+	// Check the form is within the html.
+	dialIDFound := false
+	tokenFound := false
+	doc.Find(`form[name="add-dial"]`).Each(func(index int, item *goquery.Selection) {
+		// Find dialID input within form.
+		dialID := item.Find(`input[name="dialID"]`)
+		if dialID != nil {
+			dialIDFound = true
+		}
+		// Find token input within form.
+		token := item.Find(`input[name="token"]`)
+		if token != nil {
+			tokenFound = true
+		}
+	})
+
+	is.True(dialIDFound) // dialID input element found.
+	is.True(tokenFound)  // token input element found.
+
+}
+
+func TestGettingBoardServiceError(t *testing.T) {
+
+	for _, tt := range []struct {
+		msg    string
+		err    error
+		expMsg string
+	}{{
+		msg:    "board not found",
+		err:    ooohh.ErrBoardNotFound,
+		expMsg: "Oops, the board wasn&#39;t found.",
+	}, {
+		msg:    "unknown error",
+		err:    errors.New("oops"),
+		expMsg: "Error retrieving board, please try again.",
+	}} {
+
+		t.Run(tt.msg, func(t *testing.T) {
+
+			is := is.New(t)
+
+			// Create a mock service.
+			s := &mock.Service{
+				GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+					return nil, tt.err
+				},
+			}
+
+			// Create the ui struct.
+			ui := NewUI(s)
+
+			// Create a new request.
+			r, err := newRequest("GET", "/boards/:id", nil, httprouter.Params{{Key: "id", Value: "board-id"}})
+			is.NoErr(err)
+
+			// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+			rr := httptest.NewRecorder()
+
+			// Invoke the get board handler.
+			ui.GetBoard().ServeHTTP(rr, r)
+
+			// Check the response status code is correct.
+			is.Equal(rr.Code, http.StatusOK)
+
+			// Check the error msg is within the html.
+			body := rr.Body.String()
+			is.True(strings.Contains(body, tt.expMsg)) // error message is in the html body.
+		})
+	}
+}
+
+func TestAddingDialToBoardOK(t *testing.T) {
+
+	is := is.New(t)
+
+	now := time.Now().Truncate(time.Second)
+
+	// Board that will be returned by service.
+	board := ooohh.Board{
+		ID:    ooohh.BoardID("board-id"),
+		Name:  "Testing Board",
+		Token: "token",
+		Dials: []ooohh.Dial{
+			{
+				ID:        ooohh.DialID("dial-1"),
+				Token:     "token",
+				Name:      "dial-1",
+				Value:     10.0,
+				UpdatedAt: now,
+			},
+			{
+				ID:        ooohh.DialID("dial-2"),
+				Token:     "token",
+				Name:      "dial-2",
+				Value:     66.6,
+				UpdatedAt: now,
+			},
+		},
+		UpdatedAt: now,
+	}
+
+	// Variables that will be set within the updating of the board.
+	var setID ooohh.BoardID
+	var setToken string
+	var setDials *[]ooohh.DialID
+
+	// Create a mock service.
+	s := &mock.Service{
+		GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+			return &board, nil
+		},
+		SetBoardFn: func(ctx context.Context, id ooohh.BoardID, token string, dials []ooohh.DialID) error {
+			// Capture set values.
+			setID = id
+			setToken = token
+			setDials = &dials
+
+			// update board.
+			d := make([]ooohh.Dial, len(dials))
+			for i := range dials {
+				d[i] = ooohh.Dial{
+					ID:        dials[i],
+					Token:     "token",
+					Name:      string(dials[i]),
+					Value:     10.0,
+					UpdatedAt: now,
+				}
+			}
+			board.Dials = d
+
+			return nil
+		},
+	}
+
+	// Create the ui struct.
+	ui := NewUI(s)
+
+	// Create a new request.
+	formData := url.Values{
+		"dialID": {"dial-3"},
+		"token":  {"token"},
+	}
+	r, err := newRequest("POST", "/boards/:id", strings.NewReader(formData.Encode()), httprouter.Params{{Key: "id", Value: "board-id"}})
+	is.NoErr(err) // request creates ok.
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+	rr := httptest.NewRecorder()
+
+	// Invoke the get board handler.
+	ui.GetBoard().ServeHTTP(rr, r)
+
+	// Check the board was set.
+	is.True(s.SetBoardInvoked) // board was updated.
+
+	// Check the board was updated with the correct data.
+	is.Equal(setID, ooohh.BoardID("board-id")) // correct board was set.
+	is.Equal(setToken, "token")                // token was set correctly.
+	is.True(setDials != nil)                   // dials were set.
+	if setDials != nil {
+		is.Equal(*setDials, []ooohh.DialID{"dial-1", "dial-2", "dial-3"}) // correct dials were set.
+	}
+
+	// Check the response status code is correct.
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Check the new dial is within the html.
+	body := rr.Body.String()
+	is.True(strings.Contains(body, "dial-3")) // new dial is in the html body.
+}
+
+func TestAddingDialToBoardValidationError(t *testing.T) {
+
+	now := time.Now().Truncate(time.Second)
+
+	// Board that will be returned by service.
+	board := ooohh.Board{
+		ID:    ooohh.BoardID("board-id"),
+		Name:  "Testing Board",
+		Token: "token",
+		Dials: []ooohh.Dial{
+			{
+				ID:        ooohh.DialID("dial-1"),
+				Token:     "token",
+				Name:      "dial-1",
+				Value:     10.0,
+				UpdatedAt: now,
+			},
+			{
+				ID:        ooohh.DialID("dial-2"),
+				Token:     "token",
+				Name:      "dial-2",
+				Value:     66.6,
+				UpdatedAt: now,
+			},
+		},
+		UpdatedAt: now,
+	}
+
+	// Create a mock service.
+	s := &mock.Service{
+		GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+			return &board, nil
+		},
+	}
+
+	// Create the ui struct.
+	ui := NewUI(s)
+
+	for _, tt := range []struct {
+		msg         string
+		form        url.Values
+		errMsgs     []string
+		missingMsgs []string
+	}{{
+		msg:         "no dial id or token",
+		form:        url.Values{},
+		errMsgs:     []string{"Please enter a dial ID.", "Please enter the board&#39;s token."},
+		missingMsgs: []string{},
+	}, {
+		msg: "no dial id",
+		form: url.Values{
+			"token": {"token"},
+		},
+		errMsgs:     []string{"Please enter a dial ID."},
+		missingMsgs: []string{"Please enter the board&#39;s token."},
+	}, {
+		msg: "no token",
+		form: url.Values{
+			"dialID": {"dial-id"},
+		},
+		errMsgs:     []string{"Please enter the board&#39;s token."},
+		missingMsgs: []string{"Please enter a dial ID."},
+	}} {
+
+		t.Run(tt.msg, func(t *testing.T) {
+
+			is := is.New(t)
+
+			// Create a new request.
+			r, err := newRequest("POST", "/boards/:id", strings.NewReader(tt.form.Encode()), httprouter.Params{{Key: "id", Value: "board-id"}})
+			is.NoErr(err) // request creates ok.
+			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+			rr := httptest.NewRecorder()
+
+			// Invoke the get board handler.
+			ui.GetBoard().ServeHTTP(rr, r)
+
+			// Check the board was not set.
+			is.True(!s.SetBoardInvoked) // board was not updated.
+
+			// Check the response status code is correct.
+			is.Equal(rr.Code, http.StatusOK)
+
+			// Check the validation error is within the html.
+			body := rr.Body.String()
+			for _, msg := range tt.errMsgs {
+				is.True(strings.Contains(body, msg)) // error message is in the html body.
+			}
+
+			// Check the missing messages aren't in the html.
+			for _, msg := range tt.missingMsgs {
+				is.True(!strings.Contains(body, msg)) // error message is not in the html body.
+			}
+		})
+	}
+}
+
+func TestAddingDialToBoardGetBoardError(t *testing.T) {
+
+	is := is.New(t)
+
+	// Create a mock service.
+	s := &mock.Service{
+		GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+			return nil, errors.New("uh-oh")
+		},
+	}
+
+	// Create the ui struct.
+	ui := NewUI(s)
+
+	// Create a new request.
+	formData := url.Values{
+		"dialID": {"new-dial-id"},
+		"token":  {"entered-token"},
+	}
+	r, err := newRequest("POST", "/boards/:id", strings.NewReader(formData.Encode()), httprouter.Params{{Key: "id", Value: "board-id"}})
+	is.NoErr(err) // request creates ok.
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+	rr := httptest.NewRecorder()
+
+	// Invoke the get board handler.
+	ui.GetBoard().ServeHTTP(rr, r)
+
+	// Check the board was retrieved.
+	is.True(s.GetBoardInvoked) // board was retrieved.
+
+	// Check the response status code is correct.
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Check the html.
+	body := rr.Body.String()
+	is.True(strings.Contains(body, "Error retrieving board, please try again.")) // error msg is in the html body.
+}
+
+func TestAddingDialToBoardSetBoardError(t *testing.T) {
+
+	is := is.New(t)
+
+	// Create a mock service.
+	s := &mock.Service{
+		GetBoardFn: func(ctx context.Context, id ooohh.BoardID) (*ooohh.Board, error) {
+			return &ooohh.Board{
+				ID:        ooohh.BoardID("board-id"),
+				Token:     "token",
+				Name:      "Board",
+				Dials:     []ooohh.Dial{},
+				UpdatedAt: time.Now(),
+			}, nil
+		},
+		SetBoardFn: func(ctx context.Context, id ooohh.BoardID, token string, dials []ooohh.DialID) error {
+			return errors.New("uh-oh")
+		},
+	}
+
+	// Create the ui struct.
+	ui := NewUI(s)
+
+	// Create a new request.
+	formData := url.Values{
+		"dialID": {"new-dial-id"},
+		"token":  {"entered-token"},
+	}
+	r, err := newRequest("POST", "/boards/:id", strings.NewReader(formData.Encode()), httprouter.Params{{Key: "id", Value: "board-id"}})
+	is.NoErr(err) // request creates ok.
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create a response recorder, which satisfies http.ResponseWriter, to record the response.
+	rr := httptest.NewRecorder()
+
+	// Invoke the get board handler.
+	ui.GetBoard().ServeHTTP(rr, r)
+
+	// Check the board was retrieved.
+	is.True(s.GetBoardInvoked) // board was retrieved.
+
+	// Check the board was updated.
+	is.True(s.SetBoardInvoked) // board was updated.
+
+	// Check the response status code is correct.
+	is.Equal(rr.Code, http.StatusOK)
+
+	// Check the html.
+	body := rr.Body.String()
+	is.True(strings.Contains(body, "Error adding dial, please try again.")) // error msg is in the html body.
+	is.True(strings.Contains(body, "new-dial-id"))                          // entered dial id is still on page.
+	is.True(strings.Contains(body, "entered-token"))                        // entered token is still on page.
 }
